253ad8aac76880e991272ee76e830e17d20134a4
Libo Zeng <ringil.sword@gmail.com>
2019-07-10 07:55:55 -0400

    'all_wordications'

--- word.py
+++ word.py
@@ -4,6 +4,7 @@ Created on Mon Jul  8 18:24:48 2019
 
 @author: Libo
 """
+import json
 
 # using words list from https://github.com/dwyl/english-words
 letter_to_number = {"a": 2, "b": 2, "c": 2, "d": 3, "e": 3, "f": 3, 
@@ -19,7 +20,10 @@ number_to_letter = { 2:"abc", 3:"def", 4:"ghi", 5:"jkl",
 
 def format_phone_number(s):
     s = s.replace('-', '')
-    return '-'.join([s[:1], s[1:4], s[4:7], s[7:11]])
+    if not s[6].isdigit() and not s[7].isdigit():
+        return '-'.join([s[:1], s[1:4], s[4:11]])
+    else:
+        return '-'.join([s[:1], s[1:4], s[4:7], s[7:11]])
 
 def words_to_number(s, format=True):
     r = ""
@@ -33,8 +37,27 @@ def words_to_number(s, format=True):
     else:
         return r
 
-def number_to_words(s):
-    return s
+with open('number_to_word_map.json') as f:
+    number_to_word_map = json.load(f)
 
-assert number_to_words("1-800-724-6837") == "1-800-724-6837"
+
+def generate_possibilities(s, number_to_word_map):
+    r = []
+    for i in range(len(s)):
+        for j in range(i, len(s)):
+            if s[i:j + 1] in number_to_word_map:
+                for word in number_to_word_map[s[i:j + 1]]:
+                    r.append(format_phone_number(s[:i] + word.upper() + s[j+1:]))
+    return r
+
+def all_wordifications(s):
+    s = s.replace('-', '')
+    possibilities = generate_possibilities(s, number_to_word_map)
+    print(possibilities)
+    return possibilities
+
+def number_to_words(s):
+    return all_wordifications(s)[0]
+
+assert "1-800-PAINTER" in all_wordifications("1-800-724-6837")
 assert words_to_number("1-800-PAINTER") == "1-800-724-6837"
